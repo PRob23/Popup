85b6cf909bc09913df53adcd50e0794ec0b6bf65
Gyula Juhász <gyu999@gmail.com>
2023-09-19 01:35:46 +0200

    As expected, simply injecting an Activity instance will not work
    C:\StudioProjects\PopupGenyo\app\build\generated\hilt\component_sources\debug\com\gyulajuhasz\popupgenyo\PopupGenyoApplication_HiltComponents.java:128: error: [Dagger/MissingBinding] android.app.Activity cannot be provided without an @Inject constructor or an @Provides-annotated method.
      public abstract static class SingletonC implements PopupGenyoApplication_GeneratedInjector,

--- data/src/main/java/com/gyulajuhasz/popupgenyo/data/service/PopupServiceImpl.kt
+++ data/src/main/java/com/gyulajuhasz/popupgenyo/data/service/PopupServiceImpl.kt
@@ -1,12 +1,15 @@
 package com.gyulajuhasz.popupgenyo.data.service
 
+import android.app.Activity
 import com.gyulajuhasz.popupgenyo.domain.model.PopupResult
 import com.gyulajuhasz.popupgenyo.domain.service.PopupService
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class PopupServiceImpl
-@Inject internal constructor(): PopupService {
+@Inject internal constructor(
+    private val activity: Activity
+): PopupService {
     override suspend fun showPopupForResult(): PopupResult {
         delay(1000)
         return PopupResult.NOT_OK
