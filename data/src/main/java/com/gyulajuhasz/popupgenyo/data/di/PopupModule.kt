5988d999630c36d0f60afd4890335a3875b122e1
Gyula Juhász <gyu999@gmail.com>
2023-09-19 01:42:49 +0200

    As expected, simply installing PopupModule into ActivityComponent will not work either, dependencies bound in ViewModelComponent cannot depend on dependencies from ActivityComponent

--- data/src/main/java/com/gyulajuhasz/popupgenyo/data/di/PopupModule.kt
+++ data/src/main/java/com/gyulajuhasz/popupgenyo/data/di/PopupModule.kt
@@ -1,16 +1,25 @@
 package com.gyulajuhasz.popupgenyo.data.di
 
+import android.app.Activity
 import com.gyulajuhasz.popupgenyo.data.service.PopupServiceImpl
 import com.gyulajuhasz.popupgenyo.domain.service.PopupService
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
-@InstallIn(ViewModelComponent::class)
-interface PopupModule {
+@InstallIn(ActivityComponent::class)
+object PopupModule {
 
-    @Binds
-    fun bindPopupService(service: PopupServiceImpl): PopupService
+    @Provides
+    @ActivityScoped
+    fun provide(
+        activity: Activity
+    ): PopupService = PopupServiceImpl(
+        activity
+    )
 }
