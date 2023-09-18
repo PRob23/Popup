0f818ccbf543b195f23174f83c6163589824c7b6
Gyula Juhász <gyu999@gmail.com>
2023-09-19 01:54:17 +0200

    Reverting ActivityComponent
    This reverts commit 5988d999630c36d0f60afd4890335a3875b122e1.

--- data/src/main/java/com/gyulajuhasz/popupgenyo/data/di/PopupModule.kt
+++ data/src/main/java/com/gyulajuhasz/popupgenyo/data/di/PopupModule.kt
@@ -1,25 +1,16 @@
 package com.gyulajuhasz.popupgenyo.data.di
 
-import android.app.Activity
 import com.gyulajuhasz.popupgenyo.data.service.PopupServiceImpl
 import com.gyulajuhasz.popupgenyo.domain.service.PopupService
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
-@InstallIn(ActivityComponent::class)
-object PopupModule {
+@InstallIn(ViewModelComponent::class)
+interface PopupModule {
 
-    @Provides
-    @ActivityScoped
-    fun provide(
-        activity: Activity
-    ): PopupService = PopupServiceImpl(
-        activity
-    )
+    @Binds
+    fun bindPopupService(service: PopupServiceImpl): PopupService
 }
--- data/src/main/java/com/gyulajuhasz/popupgenyo/data/service/PopupServiceImpl.kt
+++ data/src/main/java/com/gyulajuhasz/popupgenyo/data/service/PopupServiceImpl.kt
@@ -1,15 +1,12 @@
 package com.gyulajuhasz.popupgenyo.data.service
 
-import android.app.Activity
 import com.gyulajuhasz.popupgenyo.domain.model.PopupResult
 import com.gyulajuhasz.popupgenyo.domain.service.PopupService
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class PopupServiceImpl
-@Inject internal constructor(
-    private val activity: Activity
-): PopupService {
+@Inject internal constructor(): PopupService {
     override suspend fun showPopupForResult(): PopupResult {
         delay(1000)
         return PopupResult.NOT_OK
