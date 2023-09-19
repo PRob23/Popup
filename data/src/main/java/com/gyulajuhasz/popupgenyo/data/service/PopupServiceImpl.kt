feee981bc7de4af690dbe47153c6058a8e3a39af
Gyula Juhász <gyu999@gmail.com>
2023-09-19 02:09:52 +0200

    Add an example dialog showing how to translate callbacks to suspending function call return values

--- data/src/main/java/com/gyulajuhasz/popupgenyo/data/service/PopupServiceImpl.kt
+++ data/src/main/java/com/gyulajuhasz/popupgenyo/data/service/PopupServiceImpl.kt
@@ -1,15 +1,23 @@
 package com.gyulajuhasz.popupgenyo.data.service
 
 import android.app.Activity
+import android.app.AlertDialog
 import com.gyulajuhasz.popupgenyo.domain.model.PopupResult
 import com.gyulajuhasz.popupgenyo.domain.service.PopupService
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
+import kotlin.coroutines.resume
 
 class PopupServiceImpl
-@Inject internal constructor(): PopupService {
-    override suspend fun showPopupForResult(activity: Activity): PopupResult {
-        delay(1000)
-        return PopupResult.OK
-    }
+@Inject internal constructor() : PopupService {
+    override suspend fun showPopupForResult(activity: Activity): PopupResult =
+        suspendCancellableCoroutine { continuation ->
+            AlertDialog.Builder(activity)
+                .setTitle("POPUP")
+                .setMessage("Choose your destiny!")
+                .setOnCancelListener { continuation.resume(PopupResult.NOT_OK) }
+                .setPositiveButton("COOL") { _, _ -> continuation.resume(PopupResult.OK) }
+                .setNegativeButton("GENYO") { _, _ -> continuation.resume(PopupResult.NOT_OK) }
+                .show()
+        }
 }
